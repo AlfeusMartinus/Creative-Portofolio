71a8eec02de991650ed98b493ff2f1fe9413e18f
Alfeus Martinus Sinurat <feusmartinus@gmail.com>
2023-08-15 12:13:26 +0700

    delete earthCanvas

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import { EarthCanvas, StarsCanvas } from "./canvas"
+import { StarsCanvas } from "./canvas"
 import Hero from "./Hero"
 import Navbar from "./Navbar"
 import About from "./About"
@@ -19,6 +19,5 @@ export {
   CV,
   Contact,
   Footer,
-  EarthCanvas,
   StarsCanvas,
 }
