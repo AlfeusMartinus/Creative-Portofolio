813fa0b1446ef28763b58ed4b913b1272613eca9
Alfeus Martinus Sinurat <feusmartinus@gmail.com>
2023-08-15 12:14:41 +0700

    strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { StrictMode } from 'react';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <StrictMode>
+      <App />
+    </StrictMode>
 )
